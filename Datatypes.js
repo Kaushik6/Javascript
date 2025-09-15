72831f5eafc0091811035bb806dc860f024df3dc
Kaushik6 <barikkaushik48@gmail.com>
2025-09-15 20:14:49 +0530

    Datatypes

---
+++ Datatypes.js
@@ -0,0 +1,27 @@
+"use strict";//treat all js code as newer version
+
+//alert(3+3)// we are using node js not browser
+
+console.log(3
+    +
+    3)//code readebility should be high
+console.log("Kaushik")
+
+
+let name="Kaushik"
+let age=18
+let isLoggedIn=true
+let state=null
+
+//number - 2 to power 53
+//bigint
+//string - "" '' ``
+//boolean - true/false
+//null - standalone value
+//undefined - value not defined
+//symbol - unique
+
+//object
+
+console.log(typeof null)// its an object but actually null
+console.log(typeof undefined)// its undefined
